21f219cc838ab21b41f9268f49b47b17a32b19b1
Nuray <242254@astanait.edu.kz>
2026-03-31 14:55:07 +0500

    feat: add SQL migration scripts for database initialization

---
+++ migrations/order/000001_init_orders.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE orders (
+    id TEXT PRIMARY KEY,
+    customer_id TEXT NOT NULL,
+    item_name TEXT NOT NULL,
+    amount BIGINT NOT NULL,
+    status TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP [cite: 63]
+);
